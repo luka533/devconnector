201140955e61e01b70d6b24430538595417b5c65
luka533 <simonovicluka013@outlook.de>
2026-09-06 14:20:51 +0200

    removed unused variables

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -29,6 +29,7 @@ function CommentItem({
             onClick={() => deleteCommentMutation({ postId, commentId: _id })}
             type="button"
             className="btn btn-danger"
+            disabled={isLoading}
           >
             <FaTimes />
           </button>
