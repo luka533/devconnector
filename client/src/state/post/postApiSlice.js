eec0cfba3ad9e6405a5cb4091a8a8f57d0309bf6
luka533 <simonovicluka013@outlook.de>
2026-09-06 14:49:21 +0200

    configure api routing

--- client/src/state/post/postApiSlice.js
+++ client/src/state/post/postApiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { createAlertHandler } from "../../util/queryAlertHandler";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "/api",
   credentials: "include",
 });
 
